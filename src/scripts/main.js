5e3e584d32f5f7607d1ac3f34d53b3f78ed20cdc
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-08-16 23:42:47 +0300

    promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+function getSuccess() {
+  const mainContainer = document.createElement('div');
+
+  mainContainer.classList.add('message');
+  mainContainer.textContent = 'Promise was resolved!';
+  document.body.appendChild(mainContainer);
+}
+
+function getError() {
+  const mainContainer = document.createElement('div');
+
+  mainContainer.classList.add('message', 'error-message');
+  mainContainer.textContent = 'Promise was rejected!';
+  document.body.appendChild(mainContainer);
+}
+
+logo.addEventListener('click', () => {
+  new Promise((resolve) => resolve()).then(getSuccess).catch(getError);
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+})
+  .then(getSuccess)
+  .catch(getError);
